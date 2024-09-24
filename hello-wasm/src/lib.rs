31c7edba6ae813394fc5343a80dd2cb11e12a7b8
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2024-09-24 12:34:56 -0700

    adds wee_alloc optionality to lib

--- hello-wasm/src/lib.rs
+++ hello-wasm/src/lib.rs
@@ -2,6 +2,11 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+//when enabled, use 'wee_alloc' as global allocator
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
